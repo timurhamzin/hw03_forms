373fc13d02a1c2adc3a49555ea58ca13095c38e1
Timur <timur.hamzin@gmail.com>
2020-06-24 16:58:24 +0500

    Added conftest.py in the root folder

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-sys.path.insert(0,
-                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+inserted_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, inserted_path)
 
 pytest_plugins = [
     'tests.fixtures.fixture_user',
