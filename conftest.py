77390d1a941ca9dc8e4c85525fb0d98bd57d0c38
Timur <timur.hamzin@gmail.com>
2020-06-24 17:52:30 +0500

    Modified conftest.py in tests folder

--- conftest.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-inserted_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-inserted_path = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, inserted_path)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
+myPath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, myPath)
 
 pytest_plugins = [
     'tests.fixtures.fixture_user',
