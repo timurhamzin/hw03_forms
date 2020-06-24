16a23790e043c28512bd84cc2bb3ad5171727814
Timur <timur.hamzin@gmail.com>
2020-06-24 16:32:26 +0500

    Edited conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 pytest_plugins = [
     'tests.fixtures.fixture_user',
     'tests.fixtures.fixture_data',
