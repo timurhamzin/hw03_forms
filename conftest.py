d53ba1e4c1883da6e34a1587f792b415f51919ce
Timur <timur.hamzin@gmail.com>
2020-06-24 17:31:06 +0500

    Edited conftest.py in the root folder

--- conftest.py
+++ conftest.py
@@ -2,4 +2,5 @@ import os
 import sys
 
 inserted_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+inserted_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, inserted_path)
